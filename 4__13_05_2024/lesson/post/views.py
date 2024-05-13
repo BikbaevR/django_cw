37c7b464299f8b91a93517c33d986dc3bc8a4446
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-13 21:35:05 +0500

    filters

--- 4__13_05_2024/lesson/post/views.py
+++ 4__13_05_2024/lesson/post/views.py
@@ -12,9 +12,12 @@ def index(request):
 
     print(Post.objects.filter(category=category))
     print(category.posts.all())
+    # print(category.post_set.all())
 
     # print(Post.objects.filter(category__name='текст')) У которого поле равно...
-
+    Post.objects.filter(title__startswith='строка')
+    # Post.objects.filter(title__contains='строка')
+    # Post.objects.filter(title__icontains='строка')
     # for post in posts:
     #     print(post.title)
     #     print(post.category.name)
