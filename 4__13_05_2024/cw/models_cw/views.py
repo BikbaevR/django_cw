4ccd3a53e23480618505d8730bf827e18ae557e1
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-13 21:49:04 +0500

    cw pochti zakonchil

--- 4__13_05_2024/cw/models_cw/views.py
+++ 4__13_05_2024/cw/models_cw/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from .models import Child, Kiosk
+
 
 # Create your views here.
+
+
+def index(request):
+
+    childs = Child.objects.all()
+    kiosk = Kiosk.objects.all()
+
+    return render(request, 'index.html', context={'childs': childs, 'kiosk': kiosk})
--- 4__13_05_2024/lesson/post/views.py
+++ 4__13_05_2024/lesson/post/views.py
@@ -5,21 +5,21 @@ from .models import Post, Category
 
 def index(request):
 
-    posts = Post.objects.filter(post_is_active=True, status = 'n')
-
-    category = Category.objects.get(pk=1)
-
-
-    print(Post.objects.filter(category=category))
-    print(category.posts.all())
-    # print(category.post_set.all())
-
-    # print(Post.objects.filter(category__name='текст')) У которого поле равно...
-    Post.objects.filter(title__startswith='строка')
-    # Post.objects.filter(title__contains='строка')
-    # Post.objects.filter(title__icontains='строка')
-    # for post in posts:
-    #     print(post.title)
-    #     print(post.category.name)
-
+    # posts = Post.objects.filter(post_is_active=True, status = 'n')
+    #
+    # category = Category.objects.get(pk=1)
+    #
+    #
+    # print(Post.objects.filter(category=category))
+    # print(category.posts.all())
+    # # print(category.post_set.all())
+    #
+    # # print(Post.objects.filter(category__name='текст')) У которого поле равно...
+    # Post.objects.filter(title__startswith='строка')
+    # # Post.objects.filter(title__contains='строка')
+    # # Post.objects.filter(title__icontains='строка')
+    # # for post in posts:
+    # #     print(post.title)
+    # #     print(post.category.name)
     return HttpResponse('')
+
