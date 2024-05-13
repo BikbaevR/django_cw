01cbffb72bf05b5dbd93123846db1594af95bc4b
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-13 20:16:49 +0500

    read from model

--- 4__13_05_2024/lesson/post/admin.py
+++ 4__13_05_2024/lesson/post/admin.py
@@ -6,7 +6,7 @@ from .models import Post, Category
 
 
 class AdminPost(admin.ModelAdmin):
-    list_display = ['title', 'published', 'rating']
+    list_display = ['title', 'published', 'rating', 'post_is_active']
     search_fields = ['title', 'published', 'rating']
     list_filter = ['published']
 
--- 4__13_05_2024/lesson/post/views.py
+++ 4__13_05_2024/lesson/post/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
 # Create your views here.
+from .models import Post
+
+def index(request):
+
+    posts = Post.objects.all()
+
+    for post in posts:
+        print(post.title)
+        print(post.category.name)
+
+    return HttpResponse('')
