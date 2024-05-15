a09cbb68be9bd9832e5233c845e6c6ebd96b2a0a
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-15 19:40:57 +0500

    added custom url path

--- 5__15_05_2024/lesson/post/urls.py
+++ 5__15_05_2024/lesson/post/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from .views import index, about, contact
 
 urlpatterns = [
-    path('', index),
-    path('about', about),
-    path('contact', contact)
+    path('', index, name='index_page'),
+    path('about', about, name='about_page'),
+    path('contact', contact, name='contact_page')
 ]
