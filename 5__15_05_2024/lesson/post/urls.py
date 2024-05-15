7574bc8b272cb7ff54c6f262f24d3c4fa8af4df3
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-15 21:24:00 +0500

    cw

--- 5__15_05_2024/lesson/post/urls.py
+++ 5__15_05_2024/lesson/post/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path, re_path
-from .views import index, about, contact, publication
+from .views import index, about, contact, publication, delete_post, category_list, category_from_id
 
 urlpatterns = [
     path('', index, name='index_page'),
     path('about', about, name='about_page'),
     path('contact', contact, name='contact_page'),
-    re_path(r'^post', publication),
+    path('post/info/<int:id>', publication),
+    path('post/delete/<int:id>', delete_post),
+    path('categories', category_list, name = 'categories'),
+    path('post/categories/<int:id>', category_from_id, name = 'categ_id')
+    # re_path(r'^post', publication),
 ]
--- 5__15_05_2024/lesson/post/views.py
+++ 5__15_05_2024/lesson/post/views.py
@@ -1,7 +1,7 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, Category
 
 
 # Create your views here.
@@ -11,6 +11,7 @@ def index(request):
     posts = Post.objects.all()
 
     # Post.objects.get(pk = 1).delete()
+    # get_object_or_404(Post, title='asdas')
 
     return render(request, 'post/index.html', context={'posts': posts})
 
@@ -23,5 +24,24 @@ def contact(request):
     return render(request, 'post/contact.html')
 
 
-def publication(request):
-    return HttpResponse('')
+def publication(request, id: int):
+    posts = Post.objects.get(pk=id)
+    return render(request, 'post/post.html', context={'post': posts})
+
+
+def delete_post(request, id: int):
+    post = Post.objects.get(pk=id).delete()
+    posts = Post.objects.all()
+
+    return render(request, 'post/index.html', context={'posts': posts})
+
+
+def category_list(request):
+    categories = Category.objects.all()
+    return render(request, 'post/category.html', context={'categories': categories})
+
+
+def category_from_id(request, id: int):
+    categories = Category.objects.get(pk=id)
+    posts = Post.objects.filter(category=categories)
+    return render(request, 'post/category_from_id.html', context={'posts': posts})
