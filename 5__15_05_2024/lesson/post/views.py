1fade8d8d311d73a4cc697340757b4b2476f9511
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-15 19:24:59 +0500

    added three pages

--- 5__15_05_2024/lesson/post/views.py
+++ 5__15_05_2024/lesson/post/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
 
+
 # Create your views here.
+
+
+def index(request):
+    return render(request, 'post/index.html')
+
+
+def about(request):
+    return render(request, 'post/about.html')
+
+
+def contact(request):
+    return render(request, 'post/contact.html')
