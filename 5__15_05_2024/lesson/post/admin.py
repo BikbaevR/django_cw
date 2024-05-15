da477caa2078ef64686e1adfd2f223d18d0c5555
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-15 19:58:11 +0500

    registered in admin

--- 5__15_05_2024/lesson/post/admin.py
+++ 5__15_05_2024/lesson/post/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
 
+from .models import Category, Post
+
+
 # Register your models here.
+
+class CategoryPost(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class PostPost(admin.ModelAdmin):
+    list_display = ['title', 'published_at']
+    search_fields = ['title']
+    list_filter = ['title', 'published_at', 'category']
+
+
+admin.site.register(Category, CategoryPost)
+admin.site.register(Post, PostPost)
--- 5__15_05_2024/lesson/post/models.py
+++ 5__15_05_2024/lesson/post/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        verbose_name = 'Категорию'
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.CharField(max_length=255)
+    published_at = models.DateTimeField(auto_now_add=True)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    class Meta:
+        verbose_name = 'Статью'
+        verbose_name_plural = 'Статьи'
+        ordering = ['id']
+
+    def __str__(self):
+        return self.title
