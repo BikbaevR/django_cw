eaa009deba7fce671b1070323ed25ebeffcf11fd
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-20 21:04:53 +0500

    add/get data from/to model

--- 6__20_05_2024/lesson/post/views.py
+++ 6__20_05_2024/lesson/post/views.py
@@ -26,9 +26,44 @@ def index(request):
     # post = Post.objects.create(title='title 2', category=cat)
     # print(post)
 
-    post = Post.objects.get(pk=1)
-    post.description = 'TEST'
-    post.content = 'TEEEEEEEEEEEEEST'
-    post.save()
+    # post = Post.objects.get(pk=1)
+    # post.description = 'TEST'
+    # post.content = 'TEEEEEEEEEEEEEST'
+    # post.save()
+
+    # post, _ = Post.objects.get_or_create(title="title new", category=cat)
+    # post, _ = Post.objects.get_or_create(title="title new2", defaults={
+    #     "category": cat,
+    #     'description': 'eeeeeeeeeeeeeeeeeeeeeeee'
+    # })
+
+    # post, _ = Post.objects.update_or_create(title='title new', defaults={
+    #     "content": 'sssssssssssssss',
+    #     'is_published': True
+    # })
+    #
+    # print(post)
+    # post.description = 'asdasdasdasdsa'
+    # post.save()
+
+    data1 = [
+        {
+            'title': 'title25',
+            'category': cat,
+        }
+    ]
+
+
+
+    # data = [
+    #     Post(title='title6', category=cat),
+    #     Post(title='title7', category=cat),
+    #     Post(title='title8', category=cat),
+    #     Post(title='title9', category=cat),
+    # ]
+
+    data = list(map(lambda i: Post(**i), data1))
+
+    Post.objects.bulk_create(data)
 
     return HttpResponse("success")
