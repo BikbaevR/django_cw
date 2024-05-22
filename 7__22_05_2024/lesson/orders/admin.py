38bc2c4e2ceb9967cd9b5621c73e68b965674fbc
BikbaevR <rafael.bikbayev@gmail.com>
2024-05-22 19:32:08 +0500

    lesson 7 added model

---
+++ 7__22_05_2024/lesson/orders/admin.py
@@ -0,0 +1,27 @@
+from django.contrib import admin
+from .models import Category, Product, Order
+
+
+# Register your models here.
+
+class AdminCategory(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class AdminProduct(admin.ModelAdmin):
+    list_display = ['name']
+    search_fields = ['name']
+    list_filter = ['name']
+
+
+class AdminOrder(admin.ModelAdmin):
+    list_display = ['user', 'created']
+    search_fields = ['user']
+    list_filter = ['user', 'created']
+
+
+admin.site.register(Category, AdminCategory)
+admin.site.register(Product, AdminProduct)
+admin.site.register(Order, AdminOrder)
---
+++ 7__22_05_2024/lesson/orders/models.py
@@ -0,0 +1,47 @@
+from django.db import models
+
+
+# Create your models here.
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=70)
+    slug = models.SlugField(unique=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Категория"
+        verbose_name_plural = 'Категории'
+        ordering = ['id']
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(unique=True)
+    price = models.FloatField(default=0)
+    quantity = models.PositiveIntegerField(default=0)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = "Продукт"
+        verbose_name_plural = 'Продукты'
+        ordering = ['id']
+
+
+class Order(models.Model):
+    user = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+    products = models.ManyToManyField(Product)
+
+    def __str__(self):
+        return self.id
+
+    class Meta:
+        verbose_name = "Заказ"
+        verbose_name_plural = 'Заказы'
+        ordering = ['id']
