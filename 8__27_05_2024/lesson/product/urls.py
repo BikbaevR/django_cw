981c3eda592a12810d0f34ef38602d64b3771750
r.bikbaev <r.bikbaev@foranx.com>
2024-06-04 11:32:26 +0500

    lesson 8 revision

--- 8__27_05_2024/lesson/product/urls.py
+++ 8__27_05_2024/lesson/product/urls.py
@@ -1,9 +1,13 @@
 from django.urls import path
-from .views import index, store, add_categ, change_categ
+from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
     path('store/<int:store_id>', store, name='store'),
     path('add', add_categ, name='add'),
-    path('change_categ/<int:categ_id>', change_categ, name='change_categ')
+    path('change_categ/<int:categ_id>', change_categ, name='change_categ'),
+    path('change_categ_confirm', change_categ_confirm, name='change_categ_confirm'),
+    path('delete_categ/<int:categ_id>', delete_categ, name='delete_categ'),
+    path('view_posts_categ/<int:categ_id>', view_posts_categ, name='view_posts_categ'),
+
 ]
--- 8__27_05_2024/lesson/product/views.py
+++ 8__27_05_2024/lesson/product/views.py
@@ -17,7 +17,7 @@ def index(request):
 def store(request, store_id):
     store = get_object_or_404(Store, pk=store_id)
     category = Category.objects.all()
-    print(category)
+    # print(category)
 
 
     cntx = {
@@ -30,7 +30,7 @@ def store(request, store_id):
 
 def add_categ(request):
     categ_name = request.POST.get('store_name')
-    print(categ_name)
+    # print(categ_name)
 
     category = Category()
     category.name = categ_name
@@ -42,8 +42,35 @@ def add_categ(request):
 
 
 def change_categ(request, categ_id):
-    categ_ids = request.POST.get('categ_id')
-    category = get_object_or_404(Category, pk=categ_ids)
-    category.name = request.POST.get('new_categ_name')
+
+    category = get_object_or_404(Category, pk=categ_id)
+    category_id = categ_id
+
+    cntx = {
+        'category': category,
+        'id': category_id
+    }
+
+    return render(request, 'product/change_categ.html', context=cntx)
+
+def change_categ_confirm(request):
+
+    category_id = request.POST.get('category_id')
+    new_category_name = request.POST.get('category_name')
+
+    print('aasdasds' + category_id)
+
+    category = get_object_or_404(Category, pk=category_id)
+    category.name = new_category_name
     category.save()
+
     return redirect('index')
+
+
+def delete_categ(request, categ_id):
+    category = get_object_or_404(Category, pk=categ_id)
+    category.delete()
+    return redirect('index')
+
+def view_posts_categ(request, categ_id):
+    posts =
