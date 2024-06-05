078d9400601e6727bef5e839b964505404bd11f9
BikbaevR <rafael.bikbayev@gmail.com>
2024-06-05 19:26:08 +0500

    lesson 11 make upload img

--- 11__05_06_2024/lesson/lesson/settings.py
+++ 11__05_06_2024/lesson/lesson/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_ROOT = BASE_DIR / 'media/' #для создания папки
+MEDIA_URL = 'media/' #ссылка для доступа к этой папки
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
