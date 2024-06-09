b318bb8146c0741987ca0d0a4227ddbbc0be7db8
BikbaevR <rafael.bikbayev@gmail.com>
2024-06-09 15:00:44 +0500

    lesson 8 is done

--- 8__27_05_2024/lesson/product/urls.py
+++ 8__27_05_2024/lesson/product/urls.py
@@ -9,5 +9,8 @@ urlpatterns = [
     path('change_categ_confirm', change_categ_confirm, name='change_categ_confirm'),
     path('delete_categ/<int:categ_id>', delete_categ, name='delete_categ'),
     path('view_posts_categ/<int:categ_id>', view_posts_categ, name='view_posts_categ'),
-
+    path('add_product', add_product, name='add_product'),
+    path('update_product/<int:id>', update_product, name='update_product'),
+    path('update_product_confirm', update_product_confirm, name='update_product_confirm'),
+    path('delete_product/<int:id>', delete_product, name='delete_product'),
 ]
--- 8__27_05_2024/lesson/product/views.py
+++ 8__27_05_2024/lesson/product/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect, get_object_or_404
 from django.http import HttpResponse
 from django.utils.text import slugify
 
-from .models import Store, Category
+from .models import Store, Category, Product
 
 
 # Create your views here.
@@ -18,7 +18,6 @@ def store(request, store_id):
     store = get_object_or_404(Store, pk=store_id)
     category = Category.objects.all()
     # print(category)
-
 
     cntx = {
         'category': category,
@@ -42,7 +41,6 @@ def add_categ(request):
 
 
 def change_categ(request, categ_id):
-
     category = get_object_or_404(Category, pk=categ_id)
     category_id = categ_id
 
@@ -53,8 +51,8 @@ def change_categ(request, categ_id):
 
     return render(request, 'product/change_categ.html', context=cntx)
 
-def change_categ_confirm(request):
 
+def change_categ_confirm(request):
     category_id = request.POST.get('category_id')
     new_category_name = request.POST.get('category_name')
 
@@ -72,5 +70,58 @@ def delete_categ(request, categ_id):
     category.delete()
     return redirect('index')
 
+
 def view_posts_categ(request, categ_id):
-    posts =
+    categ = Category.objects.all()
+    category = get_object_or_404(Category, pk=categ_id)
+    products = Product.objects.filter(category=category)
+    return render(request, 'product/view_post_categ.html', context={'products': products, "categ":categ})
+
+
+
+
+def add_product(request):
+    categ_id = request.POST.get('categ_id')
+    name = request.POST.get('name')
+    price = request.POST.get('price')
+    slug = request.POST.get('slug')
+
+    category = Category.objects.get(pk=categ_id)
+
+    product = Product(name=name, price=price, category=category, slug=slug)
+    product.save()
+
+    return redirect('index')
+
+
+
+def update_product(request, id):
+
+    product = get_object_or_404(Product, pk=id)
+    category = Category.objects.all()
+
+    return render(request, 'product/update_product.html', context={'product': product, "category":category})
+
+
+def update_product_confirm(request):
+    product_id = request.POST.get('product_id')
+    categ_id = request.POST.get('categ_id')
+    name = request.POST.get('name')
+    price = request.POST.get('price')
+    slug = request.POST.get('slug')
+    category = Category.objects.get(pk=categ_id)
+
+    product = Product.objects.get(pk=product_id)
+    product.name = name
+    product.price = price
+    product.category = category
+    product.slug = product.slug
+    product.save()
+
+    return redirect('index')
+
+
+def delete_product(request, id):
+    product = get_object_or_404(Product, pk=id)
+    product.delete()
+    return redirect('index')
