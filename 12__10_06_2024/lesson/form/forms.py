e372256d6965b2486da48e6b6de09a6c7d5e3b41
BikbaevR <rafael.bikbayev@gmail.com>
2024-06-10 19:46:25 +0500

    lesson 12 create form with py

--- 12__10_06_2024/lesson/form/forms.py
+++ 12__10_06_2024/lesson/form/forms.py
@@ -1,6 +1,6 @@
 from django import forms
 
 class MyForm(forms.Form):
-    name = forms.CharField()
-    age = forms.IntegerField()
-    email = forms.EmailField()
+    name = forms.CharField(max_length=10, label="Имя")
+    age = forms.IntegerField(label="Возраст")
+    email = forms.EmailField(required=False, label="Почта")
--- 12__10_06_2024/lesson/form/views.py
+++ 12__10_06_2024/lesson/form/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
+from .forms import MyForm
+
 
 # Create your views here.
+
+def index(request):
+    if request.method == 'POST':
+        form = MyForm(request.POST)
+        if form.is_valid():
+            print(form.cleaned_data['name'])
+            print(form.cleaned_data['age'])
+            print(form.cleaned_data['email'])
+
+    form = MyForm()
+    cntx = {
+        "form": form,
+    }
+
+    return render(request, 'forms/index.html', cntx)
