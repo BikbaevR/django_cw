d4f97c722081a53762c5fe9c89cb4b6837aa7385
BikbaevR <rafael.bikbayev@gmail.com>
2024-06-10 20:01:47 +0500

    lesson 12

--- 12__10_06_2024/lesson/form/models.py
+++ 12__10_06_2024/lesson/form/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
 # Create your models here.
+
+class Student(models.Model):
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    email = models.EmailField()
+
+    class Meta:
+        verbose_name = 'Студент'
+        verbose_name_plural = 'Студенты'
+
+
+    def __str__(self):
+        return self.name
--- 12__10_06_2024/lesson/form/views.py
+++ 12__10_06_2024/lesson/form/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import MyForm
 
+from .models import Student
+
 
 # Create your views here.
 
@@ -8,9 +10,16 @@ def index(request):
     if request.method == 'POST':
         form = MyForm(request.POST)
         if form.is_valid():
-            print(form.cleaned_data['name'])
-            print(form.cleaned_data['age'])
-            print(form.cleaned_data['email'])
+
+            student = Student(
+                name=form.cleaned_data['name'],
+                age=form.cleaned_data['age'],
+                email=form.cleaned_data['email']
+            )
+            student.save()
+            # print(form.cleaned_data['name'])
+            # print(form.cleaned_data['age'])
+            # print(form.cleaned_data['email'])
 
     form = MyForm()
     cntx = {
