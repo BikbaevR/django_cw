e209c09e526ef712eb1aec2b8e50f2b527d9acda
BikbaevR <rafael.bikbayev@gmail.com>
2024-06-19 21:17:17 +0500

    lesson 14

--- 14__19_06_2024/lesson/hotel/urls.py
+++ 14__19_06_2024/lesson/hotel/urls.py
@@ -3,4 +3,6 @@ from .views import *
 
 urlpatterns = [
     path('', index, name='index'),
+    path('create_room_type', create_room_type, name='create_room_type'),
+    path('create_room', create_room, name='create_room'),
 ]
--- 14__19_06_2024/lesson/hotel/views.py
+++ 14__19_06_2024/lesson/hotel/views.py
@@ -1,6 +1,57 @@
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from .models import *
 
 def index(request):
 
     return render(request, 'hotel/index.html')
+
+
+def create_room_type(request):
+    if request.method == 'POST':
+        room_type = request.POST['room_type']
+        room = RoomType(name=room_type)
+        room.save()
+        return redirect('create_room_type')
+
+    context = {'room_type': RoomType.objects.all()}
+    return render(request, 'hotel/create_room_type.html', context)
+
+
+def create_room(request):
+    if request.method == 'POST':
+        number = request.POST['number']
+        type = request.POST['type']
+        price = request.POST['price']
+        description = request.POST['description']
+        photo = request.FILES['photo']
+
+        room_type = RoomType.objects.get(pk=type)
+        new_room = HotelRoom(number=number, type=room_type, price=price, description=description, photo=photo)
+        new_room.save()
+        return redirect('create_room')
+
+    context = {'rooms': HotelRoom.objects.all(),
+               'room_types': RoomType.objects.all()}
+    return render(request, 'hotel/create_room.html', context)
+
+
+
+def create_reservation(request):
+    if request.method == 'POST':
+        number = request.POST['number']
+        date_start = request.POST['date_start']
+        date_end = request.POST['date_end']
+        user = request.POST['user']
+
+        room_obj = Room.objects.get(pk=number)
+        user_obj = User.objects.get(pk=user)
+
+        new_reservation = HotelReservation(number=room_obj, date_start=date_start, date_end=date_end, user=user_obj)
+        new_reservation.save()
+        return redirect('create_reservation')
+
+    context = {'reservations': HotelReservation.objects.all(),
+               'rooms:' : HotelRoom.objects.all()}
+
+
+    return render(request, 'hotel/create_reservation.html', context)
