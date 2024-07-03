203700e1a6e76f03cec46652207b52b08ff6832d
BikbaevR <rafael.bikbayev@gmail.com>
2024-07-03 20:00:53 +0500

    lesson 18

--- 18__03_07_2024/lesson/lesson/urls.py
+++ 18__03_07_2024/lesson/lesson/urls.py
@@ -19,6 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('user/', input('user_app.urls')),
-    path('', input('headHunter.urls')),
+    path('user/', include('user_app.urls')),
+    path('', include('headHunter.urls')),
+
 ]
